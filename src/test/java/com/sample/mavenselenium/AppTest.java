08f137e5f4c0f2ae8efe74b9cb996c0d968bb0e8
ccruzm631 <ccruzm631@gmail.com>
2018-08-29 16:48:10 -0500

    Change Test

--- src/test/java/com/sample/mavenselenium/AppTest.java
+++ src/test/java/com/sample/mavenselenium/AppTest.java
@@ -4,12 +4,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.Platform;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import junit.framework.Test;
@@ -46,7 +47,13 @@ public class AppTest
     public void testApp()
     {
     	System.out.println("==>Start Test App");
-    	System.setProperty("webdriver.gecko.driver", "C:\\tools\\geckodriver.exe");
+//    	System.setProperty("webdriver.firefox.marionette", "C:\\tools\\geckodriver.exe");
+//    	instanceof 
+    	System.setProperty("webdriver.gecko.driver", "C:\\tools\\geckodriver.exe"); 
+//    	System.setProperty("webdriver.firefox.marionette", "");
+    	
+    	WebDriver driver = new FirefoxDriver();
+    	driver.get("http://10.53.29.252:8080/Asistencia/");
 		
 //    	FirefoxDriver driver = new FirefoxDriver();
 //
@@ -54,19 +61,36 @@ public class AppTest
 //		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 //		driver.close();
 
-    	FirefoxOptions options = new FirefoxOptions()
-    			.addPreference("browser.startup.page", 1)
-    			.addPreference("browser.startup.homepage", "http://10.53.29.252:8080/Asistencia/");
-    	options.setCapability("marionette", false);
+//    	FirefoxOptions options = new FirefoxOptions()
+//    			//.addPreference("browser.startup.page", 0)
+//    			.addPreference("browser.startup.homepage", "http://10.53.29.252:8080/Asistencia/")
+//    			.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe")
+//    			.setLegacy(false);
+//    	options.setLogLevel(FirefoxDriverLogLevel.TRACE);
+//    	options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+//    	options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+//    	options.setCapability("marionette", false);
+//    	options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+    	//options.setAcceptInsecureCerts(false);
     	
-		try {
-			WebDriver driver = new RemoteWebDriver(new URL("http://localhost:8089/wd/hub"), options);
+//    	FirefoxDriver ffd = new FirefoxDriver(options);
+//
+//		try {
+//			WebDriver driver = new RemoteWebDriver(new URL("http://localhost:8089/wd/hub"), ffd.getCapabilities());
+//			driver.get("http://10.53.29.252:8080/Asistencia/");
 //			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			driver.close();
-		} catch (MalformedURLException e) {
-			System.out.println(e.getMessage());
-		}
-//    	
+//			
+			WebElement objUserField = driver.findElement(By.id("numempleado"));
+			objUserField.click();	
+			
+			objUserField.sendKeys("163796");
+//			
+//			driver.close();
+//			driver.quit();
+//		} catch (MalformedURLException e) {
+//			System.out.println(e.getMessage());
+//		}
+    	
         assertTrue( true );
     }
 }
