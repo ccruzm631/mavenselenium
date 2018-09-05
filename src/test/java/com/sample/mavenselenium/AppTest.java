642c886f3bafea3b65c42abc62fe551c40c5a838
ccruzm631 <ccruzm631@gmail.com>
2018-09-05 16:51:27 -0500

    Add lines to test

--- src/test/java/com/sample/mavenselenium/AppTest.java
+++ src/test/java/com/sample/mavenselenium/AppTest.java
@@ -1,20 +1,12 @@
 package com.sample.mavenselenium;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.ImmutableCapabilities;
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -55,44 +47,26 @@ public class AppTest
     	
     	WebDriver driver = new FirefoxDriver();
     	driver.get("http://10.53.29.252:8080/Asistencia/");
+    	
     	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    	
     	WebElement objUserField = driver.findElement(By.id("numempleado"));
 		objUserField.click();	
 
 		objUserField.sendKeys("163796");
-    	
+		
+		WebElement objMasterKeyField = driver.findElement(By.id("llaveMaestra"));
+		objMasterKeyField.click();
+		objMasterKeyField.sendKeys("Cgruposalinas13");
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebElement objIngresarButton = driver.findElement(By.cssSelector("input.submit:nth-child(2)"));
+		
+		//submit: Envía los datos del formulario que contiene al elemento sobre el que invoca
+		objIngresarButton.submit();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     	driver.quit();
     }
-    
-    /**
-     * Rigourous Test :-)
-     */
-    public static void test0() {
-    	System.out.println("==>Start Test 0");
-    	System.setProperty("webdriver.gecko.driver", "C:\\tools\\geckodriver.exe");
-    	
-    	FirefoxOptions options = new FirefoxOptions()
-    			.addPreference("browser.startup.page", 1)
-    			.addPreference("browser.startup.homepage", "http://10.53.29.252:8080/Asistencia/");
-    		
-    	options.setCapability("marionette", true);
-    			
-    	
-    	try {
-			WebDriver driver = new RemoteWebDriver(new URL("http://localhost:8089/wd/hub"), options);
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			WebElement objUserField = driver.findElement(By.id("numempleado"));
-			objUserField.click();	
-
-			objUserField.sendKeys("163796");
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			driver.close();
-			driver.quit();
-			
-		} catch (MalformedURLException e) {
-			System.out.println("Error Test: "+e.getLocalizedMessage());
-		}
-    	
-    	assertTrue( true );
-    }
+   
  }
