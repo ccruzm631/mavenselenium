5e4de69882c788a268fcb442c24d0e91b12f89d6
ccruzm631 <ccruzm631@gmail.com>
2018-09-05 16:55:25 -0500

    otx

--- src/test/java/com/sample/mavenselenium/AppTest.java
+++ src/test/java/com/sample/mavenselenium/AppTest.java
@@ -1,5 +1,7 @@
 package com.sample.mavenselenium;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -7,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -37,36 +40,73 @@ public class AppTest
     }
 
     
-    public void testGK() {
-    	System.out.println("==>Start Test GK");
+//    public void testGK() {
+//    	System.out.println("==>Start Test GK");
+//    	System.setProperty("webdriver.gecko.driver", "C:\\tools\\geckodriver.exe");
+//    	
+//    	FirefoxOptions options = new FirefoxOptions();
+//    	options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+//    	options.setCapability("marionette", false);
+//    	
+//    	WebDriver driver = new FirefoxDriver();
+//    	driver.get("http://10.53.29.252:8080/Asistencia/");
+//    	
+//    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//    	
+//    	WebElement objUserField = driver.findElement(By.id("numempleado"));
+//		objUserField.click();	
+//
+//		objUserField.sendKeys("163796");
+//		
+//		WebElement objMasterKeyField = driver.findElement(By.id("llaveMaestra"));
+//		objMasterKeyField.click();
+//		objMasterKeyField.sendKeys("Cgruposalinas13");
+//		
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//		WebElement objIngresarButton = driver.findElement(By.cssSelector("input.submit:nth-child(2)"));
+//		
+//		//submit: Envía los datos del formulario que contiene al elemento sobre el que invoca
+//		objIngresarButton.submit();
+//		
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//    	driver.quit();
+//    }
+   
+    public static void test0() {
+    	System.out.println("==>Start Test 0");
     	System.setProperty("webdriver.gecko.driver", "C:\\tools\\geckodriver.exe");
     	
-    	FirefoxOptions options = new FirefoxOptions();
-    	options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-    	options.setCapability("marionette", false);
+    	FirefoxOptions options = new FirefoxOptions()
+    			.addPreference("browser.startup.page", 1)
+    			.addPreference("browser.startup.homepage", "http://10.53.29.252:8080/Asistencia/");
+    		
+    	options.setCapability("marionette", true);
+    			
     	
-    	WebDriver driver = new FirefoxDriver();
-    	driver.get("http://10.53.29.252:8080/Asistencia/");
-    	
-    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    	
-    	WebElement objUserField = driver.findElement(By.id("numempleado"));
-		objUserField.click();	
+    	try {
+			WebDriver driver = new RemoteWebDriver(new URL("http://localhost:8089/wd/hub"), options);
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			WebElement objUserField = driver.findElement(By.id("numempleado"));
+			objUserField.click();	
 
-		objUserField.sendKeys("163796");
-		
-		WebElement objMasterKeyField = driver.findElement(By.id("llaveMaestra"));
-		objMasterKeyField.click();
-		objMasterKeyField.sendKeys("Cgruposalinas13");
-		
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		WebElement objIngresarButton = driver.findElement(By.cssSelector("input.submit:nth-child(2)"));
-		
-		//submit: Envía los datos del formulario que contiene al elemento sobre el que invoca
-		objIngresarButton.submit();
-		
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    	driver.quit();
+			objUserField.sendKeys("163796");
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			
+			WebElement objMasterKeyField = driver.findElement(By.id("llaveMaestra"));
+			objMasterKeyField.click();
+			objMasterKeyField.sendKeys("Cgruposalinas13");
+			
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			WebElement objIngresarButton = driver.findElement(By.cssSelector("input.submit:nth-child(2)"));
+			
+			//submit: Envía los datos del formulario que contiene al elemento sobre el que invoca
+			objIngresarButton.submit();
+			
+			driver.close();
+			driver.quit();
+			
+		} catch (MalformedURLException e) {
+			System.out.println("Error Test: "+e.getLocalizedMessage());
+		}
     }
-   
  }
